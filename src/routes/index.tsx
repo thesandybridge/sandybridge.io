65e0245ed9fb45e5416b61f0e13c35441fa5005a
thesandybridge <mattmillerf@pm.me>
2026-02-26 16:48:53 -0500

    feat: verify content pipeline works with server functions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+import { getAllPosts } from '~/lib/content'
+
+const fetchPosts = createServerFn({ method: 'GET' }).handler(async () => {
+  return getAllPosts('blog')
+})
 
 export const Route = createFileRoute('/')({
+  loader: () => fetchPosts(),
   component: Home,
 })
 
 function Home() {
-  return <div><h1>sandybridge.io</h1></div>
+  const posts = Route.useLoaderData()
+  return (
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {posts.map((p) => (
+        <div key={p.slug}>{p.title}</div>
+      ))}
+    </div>
+  )
 }
